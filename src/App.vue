999b69364f62f706e817efc7e5393e059f9f86c8
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2022-11-27 13:35:05 +0100

    esercizio completo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp/>
+    <div class="albumContainer">
+      <div class="flexThis">
+        <AlbumComp v-for="(elem, index) in albums" :key="index" :album="elem" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import AlbumComp from './components/AlbumComp.vue';
+
+import axios from 'axios';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderComp,
+    AlbumComp,
+
+
+  },
+
+
+    data(){
+      return {
+        albums: ''
+      }
+    },
+
+    mounted(){
+      this.getAlbums();
+    },
+
+    methods: {
+      getAlbums(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+          .then((response)=>{
+            this.albums = response.data.response
+            
+          })
+      }
+    }
   }
-}
+
 </script>
 
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: #1E2D3B;
+  height: 100vh;
 }
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+
+}
+
+.albumContainer{
+  width: 80%;
+  margin: auto;
+}
+
+.flexThis{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content:center;
+  justify-content: space-between;
+}
+
 </style>
